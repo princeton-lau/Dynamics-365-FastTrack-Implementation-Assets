a4d8e4c25ace0440fe4344512b6ba0080a4c3e4b
Princeton Lau <Princeton.Lau@teck.com>
2022-07-13 15:42:50 -0600

    new build without secret

--- Analytics/CDMUtilSolution/CDMUtil_AzureFunctions/CDMUtil.cs
+++ Analytics/CDMUtilSolution/CDMUtil_AzureFunctions/CDMUtil.cs
@@ -16,6 +16,8 @@ using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Azure.EventGrid.Models;
 using System.Linq;
 using CDMUtil.Spark;
+using Microsoft.Azure.WebJobs.Extensions.ServiceBus;
+using Azure.Messaging.ServiceBus;
 
 namespace CDMUtil
 {
@@ -196,13 +198,13 @@ namespace CDMUtil
 
             // Read Manifest metadata
             log.Log(LogLevel.Information, "Reading Manifest metadata");
-          
+
             ManifestDefinitions manifestDefinitions = await ManifestReader.getManifestDefinitions(c, log);
 
             List<ManifestDefinition> manifests = manifestDefinitions.Manifests;
             dynamic adlsConfig = manifestDefinitions.Config;
 
-           // log.LogInformation(JsonConvert.SerializeObject(manifestDefinitions));
+            // log.LogInformation(JsonConvert.SerializeObject(manifestDefinitions));
             List<SQLMetadata> metadataList = new List<SQLMetadata>();
             List<SQLStatement> statementsList = new List<SQLStatement>();
 
@@ -214,7 +216,7 @@ namespace CDMUtil
                 c.rootFolder = manifest.ManifestLocation;
                 c.manifestName = $"{manifest.ManifestName}.manifest.cdm.json";
                 c.tableList = tableNames;
-               
+
                 await ManifestReader.manifestToSQLMetadata(c, metadataList, log, c.rootFolder);
 
                 log.Log(LogLevel.Information, "Converting metadata to DDL");
@@ -228,9 +230,9 @@ namespace CDMUtil
                     statementsList = await SQLHandler.sqlMetadataToDDL(metadataList, c, log);
                 }
             }
-         
+
             return new OkObjectResult(JsonConvert.SerializeObject(
-                new {CDMMetadata= metadataList, SQLStatements =statementsList}));
+                new { CDMMetadata = metadataList, SQLStatements = statementsList }));
         }
         [FunctionName("getMetadata")]
         public static async Task<IActionResult> getMetadata(
@@ -364,8 +366,8 @@ namespace CDMUtil
             string fileFormat = getConfigurationValue(req, "FileFormat", ManifestURL);
             if (fileFormat != null)
                 AppConfiguration.synapseOptions.fileFormatName = fileFormat;
-            
-            string ParserVersion = getConfigurationValue(req, "ParserVersion", ManifestURL);            
+
+            string ParserVersion = getConfigurationValue(req, "ParserVersion", ManifestURL);
             if (ParserVersion != null)
                 AppConfiguration.synapseOptions.parserVersion = ParserVersion;
 
@@ -425,6 +427,172 @@ namespace CDMUtil
             }
 
             return AppConfiguration;
+        }
+
+        public static AppConfigurations GetAppConfigurationsServiceBusTrigger(ExecutionContext context, ILogger log, string url)
+        {
+            HttpRequest req = null;
+            string ManifestURL;
+
+            if (url != null)
+            {
+                ManifestURL = url;
+            }
+            else
+            {
+                ManifestURL = getConfigurationValue(req, "ManifestURL");
+            }
+            if (ManifestURL.ToLower().EndsWith("cdm.json") == false && ManifestURL.ToLower().EndsWith("model.json") == false)
+            {
+                throw new Exception($"Invalid manifest URL:{ManifestURL}");
+            }
+            string AccessKey = getConfigurationValue(req, "AccessKey", ManifestURL);
+
+            string tenantId = getConfigurationValue(req, "TenantId", ManifestURL);
+            string connectionString = getConfigurationValue(req, "SQLEndpoint", ManifestURL);
+            string DDLType = getConfigurationValue(req, "DDLType", ManifestURL);
+
+            string targetSparkConnection = getConfigurationValue(req, "TargetSparkConnection", ManifestURL);
+
+            log.LogInformation($"connectionString={connectionString}");
+
+            AppConfigurations AppConfiguration = new AppConfigurations(tenantId, ManifestURL, AccessKey, connectionString, DDLType, targetSparkConnection);
+
+            string AXDBConnectionString = getConfigurationValue(req, "AXDBConnectionString", ManifestURL);
+
+
+            if (AppConfiguration.tableList == null)
+            {
+                string TableNames = getConfigurationValue(req, "TableNames", ManifestURL);
+                AppConfiguration.tableList = String.IsNullOrEmpty(TableNames) ? new List<string>() { "*" } : new List<string>(TableNames.Split(','));
+            }
+
+            if (AXDBConnectionString != null)
+                AppConfiguration.AXDBConnectionString = AXDBConnectionString;
+
+            string schema = getConfigurationValue(req, "Schema", ManifestURL);
+            if (schema != null)
+                AppConfiguration.synapseOptions.schema = schema;
+
+            string fileFormat = getConfigurationValue(req, "FileFormat", ManifestURL);
+            if (fileFormat != null)
+                AppConfiguration.synapseOptions.fileFormatName = fileFormat;
+
+            string ParserVersion = getConfigurationValue(req, "ParserVersion", ManifestURL);
+            if (ParserVersion != null)
+                AppConfiguration.synapseOptions.parserVersion = ParserVersion;
+
+            string TranslateEnum = getConfigurationValue(req, "TranslateEnum", ManifestURL);
+            if (TranslateEnum != null)
+                AppConfiguration.synapseOptions.TranslateEnum = bool.Parse(TranslateEnum);
+
+            string DefaultStringLength = getConfigurationValue(req, "DefaultStringLength", ManifestURL);
+
+            if (DefaultStringLength != null)
+            {
+                AppConfiguration.synapseOptions.DefaultStringLength = Int16.Parse(DefaultStringLength);
+            }
+
+            //AppConfiguration.SourceColumnProperties = Path.Combine(context.FunctionAppDirectory, "SourceColumnProperties.json");
+            //AppConfiguration.ReplaceViewSyntax = Path.Combine(context.FunctionAppDirectory, "ReplaceViewSyntax.json");
+
+
+            string ProcessEntities = getConfigurationValue(req, "ProcessEntities", ManifestURL);
+
+            if (ProcessEntities != null)
+            {
+                AppConfiguration.ProcessEntities = bool.Parse(ProcessEntities);
+                //AppConfiguration.ProcessEntitiesFilePath = Path.Combine(context.FunctionAppDirectory, "EntityList.json");
+
+            }
+            string CreateStats = getConfigurationValue(req, "CreateStats", ManifestURL);
+
+            if (CreateStats != null)
+            {
+                AppConfiguration.synapseOptions.createStats = bool.Parse(CreateStats);
+            }
+
+            string ProcessSubTableSuperTables = getConfigurationValue(req, "ProcessSubTableSuperTables", ManifestURL);
+
+            if (ProcessSubTableSuperTables != null)
+            {
+                AppConfiguration.ProcessSubTableSuperTables = bool.Parse(ProcessSubTableSuperTables);
+                //AppConfiguration.ProcessSubTableSuperTablesFilePath = Path.Combine(context.FunctionAppDirectory, "SubTableSuperTableList.json");
+
+            }
+            string ServicePrincipalBasedAuthentication = getConfigurationValue(req, "ServicePrincipalBasedAuthentication", ManifestURL);
+
+            if (ServicePrincipalBasedAuthentication != null)
+            {
+                AppConfiguration.synapseOptions.servicePrincipalBasedAuthentication = bool.Parse(ServicePrincipalBasedAuthentication);
+                if (AppConfiguration.synapseOptions.servicePrincipalBasedAuthentication)
+                {
+                    AppConfiguration.synapseOptions.servicePrincipalTenantId = tenantId;
+                    string servicePrincipalAppId = getConfigurationValue(req, "ServicePrincipalAppId", ManifestURL);
+                    if (servicePrincipalAppId != null)
+                        AppConfiguration.synapseOptions.servicePrincipalAppId = servicePrincipalAppId;
+                    string servicePrincipalSecret = getConfigurationValue(req, "ServicePrincipalSecret", ManifestURL);
+                    if (servicePrincipalSecret != null)
+                        AppConfiguration.synapseOptions.servicePrincipalSecret = servicePrincipalSecret;
+                }
+            }
+
+            return AppConfiguration;
+        }
+        [FunctionName("ServiceBusQueueTriggerPL")]
+        public static void Run(
+            [ServiceBusTrigger("sb-queue-cdmutil-fin-costmgt-predev", Connection = "AzureWebJobsServiceBus")]
+            EventGridEvent serviceBusQueueItem,
+            Int32 deliveryCount,
+            DateTime enqueuedTimeUtc,
+            string messageId,
+            ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {serviceBusQueueItem}");
+            log.LogInformation($"EnqueuedTimeUtc={enqueuedTimeUtc}");
+            log.LogInformation($"DeliveryCount={deliveryCount}");
+            log.LogInformation($"MessageId={messageId}");
+
+            dynamic eventData = serviceBusQueueItem.Data;
+            string ManifestURL = eventData.url;
+
+            log.LogInformation($"ManifestURL={ManifestURL}");
+
+            if (!ManifestURL.EndsWith(".cdm.json"))
+            {
+                log.LogWarning("Invalid manifestURL");
+                return;
+            }
+            //get configurations data 
+            AppConfigurations c = GetAppConfigurationsServiceBusTrigger(null, log, ManifestURL);
+
+            log.LogInformation($"config1={JsonConvert.SerializeObject(c, Formatting.Indented)}");
+
+            log.LogInformation(serviceBusQueueItem.ToString());
+            // Read Manifest metadata
+            log.Log(LogLevel.Information, "Reading Manifest metadata");
+            List<SQLMetadata> metadataList = new List<SQLMetadata>();
+
+            ManifestReader.manifestToSQLMetadata(c, metadataList, log, c.rootFolder);
+
+            //sometimes the JSON file is dropped before the folder path exists, wait for the folder to exist before attempting to create the table
+            // Applies only to Tables and ChangeFeed folder
+            if (ManifestURL.Contains("/Entities/") == false)
+            {
+                ManifestReader.WaitForFolderPathsToExist(c, metadataList, log).Wait();
+                log.Log(LogLevel.Information, "Reading Manifest metadata");
+            }
+
+            if (!String.IsNullOrEmpty(c.synapseOptions.targetSparkEndpoint))
+            {
+                SparkHandler.executeSpark(c, metadataList, log);
+            }
+            else
+            {
+                SQLHandler.executeSQL(c, metadataList, log);
+                log.Log(LogLevel.Information, "CDMUtil trigger executeSQL");
+            }
+            log.Log(LogLevel.Information, "CDMUtil trigger complete");
         }
     }
 }
